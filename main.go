a82e47f52ef8adc94c5fdd812768486735a9291f
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-11-22 22:27:47 +0900

    Check command existence

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/0xAX/notificator"
@@ -12,8 +13,22 @@ var notify *notificator.Notificator
 func main() {
 	message := strings.Join(os.Args[1:], "")
 
-	notify = notificator.New(notificator.Options{})
-	if err := notify.Push("", message, "", notificator.UR_CRITICAL); err != nil {
-		panic(err)
+	if canCreateDesktopNotification() {
+		notify = notificator.New(notificator.Options{})
+		if err := notify.Push("", message, "", notificator.UR_CRITICAL); err != nil {
+			panic(err)
+		}
 	}
+}
+
+func canCreateDesktopNotification() bool {
+	if _, err := exec.LookPath("notify-send"); err == nil {
+		return true
+	}
+
+	if _, err := exec.LookPath("osascript"); err == nil {
+		return true
+	}
+
+	return false
 }
